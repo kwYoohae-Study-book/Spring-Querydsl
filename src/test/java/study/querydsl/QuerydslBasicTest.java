a25662a83e635fbaa8c5f3e3b8917d3a86c8f32b
kwYoohae <gocksdl7979@gmail.com>
2023-05-17 16:02:51 +0900

    [기본 문법] : Case 문

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -19,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -411,6 +412,34 @@ public class QuerydslBasicTest {
 
 		for (Tuple tuple : fetch) {
 			System.out.println("tuple = " + tuple);
+		}
+	}
+
+	@Test
+	void basicCase() {
+		List<String> result = queryFactory
+			.select(member.age.when(10).then("열살")
+				.when(20).then("스무살")
+				.otherwise("기타")
+			).from(member).fetch();
+
+		for (String s : result) {
+			System.out.println("s = " + s);
+		}
+	}
+
+	@Test
+	void complexCase() {
+		List<String> result = queryFactory
+			.select(new CaseBuilder()
+				.when(member.age.between(0, 20)).then("0~20살")
+				.when(member.age.between(21, 30)).then("21-30살")
+				.otherwise("기타"))
+			.from(member)
+			.fetch();
+
+		for (String s : result) {
+			System.out.println("s = " + s);
 		}
 	}
 }
