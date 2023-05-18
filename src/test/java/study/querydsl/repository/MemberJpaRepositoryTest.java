e9b4ed7a8d87b613439d679965ebfede24d02d6c
kwYoohae <gocksdl7979@gmail.com>
2023-05-18 14:41:09 +0900

    [실무 활용 - 순수 JPA와 Querydsl] 동적 쿼리와 성능 최적화 조회 - Where절 파라미터 사용

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -87,4 +87,31 @@ class MemberJpaRepositoryTest {
 		List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
 		assertThat(result).extracting("username").containsExactly("member3","member4");
 	}
+
+	@Test
+	void searchWhereTest() {
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		em.persist(teamA);
+		em.persist(teamB);
+
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 20, teamA);
+
+		Member member3 = new Member("member3", 30, teamB);
+		Member member4 = new Member("member4", 40, teamB);
+		em.persist(member1);
+		em.persist(member2);
+		em.persist(member3);
+		em.persist(member4);
+
+		// 기본조건 및 페이징 쿼리가 존재해야함, limit이라도 줘야함 -> 모든 조건이 없는경우 모든 데이터를 불러오기 때문
+		MemberSearchCondition condition = new MemberSearchCondition();
+		condition.setAgeGoe(35);
+		condition.setAgeLoe(40);
+		condition.setTeamName("teamB");
+
+		List<MemberTeamDto> result = memberJpaRepository.search(condition);
+		assertThat(result).extracting("username").containsExactly("member4");
+	}
 }
