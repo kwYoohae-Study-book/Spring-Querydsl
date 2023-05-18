9e9d90bcdf4c4c88ce9f4689fce10a3da88a6ba2
kwYoohae <gocksdl7979@gmail.com>
2023-05-18 14:32:07 +0900

    [실무 활용 - 순수 JPA와 Querydsl] 동적 쿼리와 성능 최적화 조회 - Builder 사용

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -8,12 +8,18 @@ import java.util.List;
 import javax.persistence.EntityManager;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 @SpringBootTest
 @Transactional
@@ -53,5 +59,32 @@ class MemberJpaRepositoryTest {
 
 		List<Member> result2 = memberJpaRepository.findByUsername_Querydsl("member1");
 		assertThat(result2).containsExactly(member);
+	}
+
+	@Test
+	void searchTest() {
+		Team teamA = new Team("teamA");
+		Team teamB = new Team("teamB");
+		em.persist(teamA);
+		em.persist(teamB);
+
+		Member member1 = new Member("member1", 10, teamA);
+		Member member2 = new Member("member2", 20, teamA);
+
+		Member member3 = new Member("member3", 30, teamB);
+		Member member4 = new Member("member4", 40, teamB);
+		em.persist(member1);
+		em.persist(member2);
+		em.persist(member3);
+		em.persist(member4);
+
+		// 기본조건 및 페이징 쿼리가 존재해야함, limit이라도 줘야함 -> 모든 조건이 없는경우 모든 데이터를 불러오기 때문
+		MemberSearchCondition condition = new MemberSearchCondition();
+		// condition.setAgeGoe(20);
+		// condition.setAgeLoe(40);
+		// condition.setTeamName("teamB");
+
+		List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+		assertThat(result).extracting("username").containsExactly("member3","member4");
 	}
 }
