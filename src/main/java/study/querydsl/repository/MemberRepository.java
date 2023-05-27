cb2bc320b760b8fc5ed72be7db6459ce8cdfb470
kwYoohae <gocksdl7979@gmail.com>
2023-05-27 15:05:38 +0900

    [실무 활용 - 스프링 데이터 JPA와 Querydsl] 사용자 정의 리포지토리

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import study.querydsl.entity.Member;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 
 	// select m from Member m where m.username = ?;
 	List<Member> findByUsername(String username);
